7b28efa6bfd362118cd37f6642d3b7b86963609f
Double A <developer.ip6@gmail.com>
2018-12-07 13:46:18 +0000

    Switch to Snap camera

--- msm8953.mk
+++ msm8953.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    Camera2 \
+    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
